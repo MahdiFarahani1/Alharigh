093af4601d256c7ac220351d25978ad46042bfe8
unknown <denversmurf01@gmail.com>
2024-12-01 13:43:19 +0330

    download_books_page

--- lib/Features/LoadedBooks/presentation/downloadedBook_page.dart
+++ lib/Features/LoadedBooks/presentation/downloadedBook_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/gen/assets.gen.dart';
 
 class DownloadedBookListPage extends StatelessWidget {
   const DownloadedBookListPage({super.key});
@@ -8,7 +9,7 @@ class DownloadedBookListPage extends StatelessWidget {
     final books = [
       {
         'title': 'فقه الخلف - طرق ثبوت الهلال 4',
-        'image': 'assets/book_cover.png', // جایگزین کنید با مسیر تصویر
+        'image': Assets.images.item.path,
         'actions': [
           Icons.delete,
           Icons.print,
@@ -19,7 +20,7 @@ class DownloadedBookListPage extends StatelessWidget {
       },
       {
         'title': 'فقه الخلف - الأمر بالمعروف القسم 1 8',
-        'image': 'assets/book_cover.png', // جایگزین کنید با مسیر تصویر
+        'image': Assets.images.item.path,
         'actions': [
           Icons.delete,
           Icons.print,
@@ -30,7 +31,7 @@ class DownloadedBookListPage extends StatelessWidget {
       },
       {
         'title': 'فقه الخلف - فقه التلقيح الصناعي 12',
-        'image': 'assets/book_cover.png', // جایگزین کنید با مسیر تصویر
+        'image': Assets.images.item.path,
         'actions': [
           Icons.delete,
           Icons.print,
@@ -41,7 +42,7 @@ class DownloadedBookListPage extends StatelessWidget {
       },
       {
         'title': 'لنتسوج من قصص الحياة',
-        'image': 'assets/life_story.png', // جایگزین کنید با مسیر تصویر
+        'image': Assets.images.item.path,
         'actions': [
           Icons.delete,
           Icons.print,
@@ -58,12 +59,20 @@ class DownloadedBookListPage extends StatelessWidget {
         itemCount: books.length,
         itemBuilder: (context, index) {
           final book = books[index];
-          return Card(
-            margin: const EdgeInsets.only(bottom: 16),
-            elevation: 4,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
+          return Container(
+            margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                border: Border(
+                    bottom: BorderSide(
+                        color: Theme.of(context)
+                            .floatingActionButtonTheme
+                            .backgroundColor!),
+                    right: BorderSide(
+                        color: Theme.of(context)
+                            .floatingActionButtonTheme
+                            .backgroundColor!))),
             child: Padding(
               padding: const EdgeInsets.all(16),
               child: Row(
@@ -77,28 +86,38 @@ class DownloadedBookListPage extends StatelessWidget {
                   const SizedBox(width: 16),
                   Expanded(
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         Text(
                           book['title'].toString(),
                           style: Theme.of(context).textTheme.bodyLarge,
                           maxLines: 2,
+                          textDirection: TextDirection.rtl,
                           overflow: TextOverflow.ellipsis,
                         ),
                         const SizedBox(height: 8),
                         Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
                           children: (book['actions'] as List<IconData>)
                               .map<Widget>((icon) {
                             return Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 4),
-                              child: IconButton(
-                                onPressed: () {},
-                                icon: Icon(icon),
-                                color: Colors.grey[600],
-                                iconSize: 24,
-                              ),
-                            );
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 2),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      color: Theme.of(context)
+                                          .floatingActionButtonTheme
+                                          .backgroundColor),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Icon(
+                                      icon,
+                                      color: Colors.white,
+                                      size: 24,
+                                    ),
+                                  ),
+                                ));
                           }).toList(),
                         ),
                       ],
